034fc28002740e3be5fbc4a1a4ceddb312b79715
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-06 21:44:30 +0700

    Context for firebase

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
 import App from "./app";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 ReactDOM.render(
 	<>
-		<App />
+		<FirebaseContext.Provider value={{ firebase }}>
+			<App />
+		</FirebaseContext.Provider>
 	</>,
 	document.getElementById("root")
 );
