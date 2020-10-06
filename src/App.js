a11f90568c530bac72340c7d0466e11499fb20a4
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-06 16:43:04 +0700

    Route pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
-import FaqsContainer from "./containers/faqs";
-import FooterContainer from "./containers/footer";
-import JumbotronContainer from "./containers/jumbotron";
+import React, { Suspense } from "react";
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { GlobalStyles } from "./global-styles";
+import * as ROUTES from "./constants/routes";
+import NotFound from "./components/NotFound";
+const Home = React.lazy(() => import("./pages/home"));
+const Browse = React.lazy(() => import("./pages/browse"));
+const Signin = React.lazy(() => import("./pages/signin"));
+const Signup = React.lazy(() => import("./pages/signup"));
+
 function App() {
 	return (
-		<>
-			<JumbotronContainer />
-			<FaqsContainer />
-			<FooterContainer />
-		</>
+		<Suspense fallback={<div>Loading...</div>}>
+			<GlobalStyles />
+
+			<Router>
+				<Route exact path={ROUTES.HOME} component={Home} />
+				<Route exact path={ROUTES.BROWSE} component={Browse} />
+				<Route exact path={ROUTES.SIGN_IN} component={Signin} />
+				<Route exact path={ROUTES.SIGN_UP} component={Signup} />
+				<Route component={NotFound} />
+			</Router>
+		</Suspense>
 	);
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "normalize.css";
 import App from "./app";
-import { GlobalStyles } from "./global-styles";
 
 ReactDOM.render(
 	<>
-		<GlobalStyles />
 		<App />
 	</>,
 	document.getElementById("root")
