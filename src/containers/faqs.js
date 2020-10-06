f9f56fd5172065f90b783fc2c8fddfe465a93617
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-06 13:49:41 +0700

    Opt-form component

--- src/containers/faqs.js
+++ src/containers/faqs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import faqsData from "../fixtures/faqs.json";
-import { Accordion } from "../components";
+import { Accordion, OptForm } from "../components";
 const FaqsContainer = () => {
 	return (
 		<Accordion>
@@ -12,6 +12,15 @@ const FaqsContainer = () => {
 				</Accordion.Item>
 			))}
 			<Accordion.Item />
+
+			<OptForm>
+				<OptForm.Input placeholder="Email address" />
+				<OptForm.Button>Try it now</OptForm.Button>
+				<OptForm.Break />
+				<OptForm.Text>
+					Ready to watch? Enter your email to create or restart your membership{" "}
+				</OptForm.Text>
+			</OptForm>
 		</Accordion>
 	);
 };
