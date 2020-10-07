53405de4ee24dfe61ae95500f3f9b7235e7f6bd8
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-07 14:53:10 +0700

    signin page

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,10 +1,77 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+
+import { FirebaseContext } from "../context/firebase";
+
+import { Form } from "../components";
+import FooterContainer from "../containers/footer";
+import HeaderContainer from "../containers/header";
+
+import * as ROUTES from "../constants/routes";
+import { useHistory } from "react-router-dom";
 
 const Signin = () => {
+	const [emailAddress, setEmailAddress] = useState("");
+	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+
+	const { firebase } = useContext(FirebaseContext);
+
+	const history = useHistory();
+
+	const isInvalid = password === "" || emailAddress === "";
+
+	const handleSignin = (event) => {
+		event.preventDefault();
+
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(emailAddress, password)
+			.then(() => {
+				// push to the browse page
+				history.push(ROUTES.BROWSE);
+			})
+			.catch((error) => {
+				setEmailAddress("");
+				setPassword("");
+				setError(error.message);
+			});
+	};
+
 	return (
-		<div>
-			<p>Signin</p>
-		</div>
+		<>
+			<HeaderContainer>
+				<Form>
+					<Form.Title>Sign In</Form.Title>
+					{error && <Form.Error>{error}</Form.Error>}
+					<Form.Base onSubmit={handleSignin} method="POST">
+						<Form.Input
+							placeholder="Email address"
+							value={emailAddress}
+							onChange={({ target }) => setEmailAddress(target.value)}
+						/>
+						<Form.Input
+							type="password"
+							placeholder="Password"
+							autoComplete="off"
+							value={password}
+							onChange={({ target }) => setPassword(target.value)}
+						/>
+						<Form.Submit disabled={isInvalid} type="submit">
+							Sign In
+						</Form.Submit>
+					</Form.Base>
+					<Form.Text>
+						New to Netflix?{" "}
+						<Form.Link to={ROUTES.SIGN_UP}> Sign up now.</Form.Link>
+					</Form.Text>
+					<Form.TextSmall>
+						This page is protected by Google reCAPTCHA to ensure you're not a
+						bot. Learn more.
+					</Form.TextSmall>
+				</Form>
+			</HeaderContainer>
+			<FooterContainer />
+		</>
 	);
 };
 
