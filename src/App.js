7fc72a18facb0f818d5593b646e94d4143a05cd1
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-07 19:49:18 +0700

    Route Auth

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Suspense } from "react";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { GlobalStyles } from "./global-styles";
-import * as ROUTES from "./constants/routes";
 import NotFound from "./components/NotFound";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
+import { IsUserRedirect } from "./helpers/routes";
+
 const Home = React.lazy(() => import("./pages/home"));
 const Browse = React.lazy(() => import("./pages/browse"));
 const Signin = React.lazy(() => import("./pages/signin"));
 const Signup = React.lazy(() => import("./pages/signup"));
 
 function App() {
+	const user = {};
+
 	return (
 		<Suspense fallback={<div>Loading...</div>}>
 			<GlobalStyles />
 
 			<Router>
 				<Switch>
+					<IsUserRedirect
+						user={user}
+						loggedInPath={ROUTES.BROWSE}
+						path={ROUTES.SIGN_IN}
+						exact
+					>
+						<Signin />
+					</IsUserRedirect>
+					<IsUserRedirect
+						user={user}
+						loggedInPath={ROUTES.BROWSE}
+						path={ROUTES.SIGN_UP}
+						exact
+					>
+						<Signup />
+					</IsUserRedirect>
 					<Route exact path={ROUTES.HOME} component={Home} />
 					<Route exact path={ROUTES.BROWSE} component={Browse} />
-					<Route exact path={ROUTES.SIGN_IN} component={Signin} />
-					<Route exact path={ROUTES.SIGN_UP} component={Signup} />
 					<Route component={NotFound} />
 				</Switch>
 			</Router>
