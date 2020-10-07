fdef34cb31bf4339d73326077883f3d8513033ee
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-07 21:58:54 +0700

    loading component

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Spinner, LockBody, Picture, ReleaseBody } from "./styles/loading";
 const Loading = ({ src, ...restProps }) => {
 	return (
-		<Spinner>
+		<Spinner {...restProps}>
 			<LockBody />
 			<Picture src={`/images/users/${src}.png`} />
 		</Spinner>
