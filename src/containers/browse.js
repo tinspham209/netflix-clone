7a7c3f89eccdec5df35b8f37400e9ac8d9d1344f
Tín Phạm <34814999+tinspham209@users.noreply.github.com>
2020-10-09 07:12:32 +0700

    Live search

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import Fuse from "fuse.js";
 import { Card, Header, Loading, Player } from "../components";
 
 import * as ROUTES from "../constants/routes";
@@ -28,6 +29,20 @@ const BrowseContainer = ({ slides }) => {
 	useEffect(() => {
 		setSlideRows(slides[category]);
 	}, [slides, category]);
+
+	useEffect(() => {
+		const fuse = new Fuse(slideRows, {
+			keys: ["data.description", "data.title", "data.genre"],
+		});
+		const results = fuse.search(searchTerm).map(({ item }) => item);
+
+		if (slideRows.length > 0 && searchTerm.length > 3 && results.length > 0) {
+			setSlideRows(results);
+		} else {
+			setSlideRows(slides[category]);
+		}
+		// eslint-disable-next-line
+	}, [searchTerm]);
 
 	return profile.displayName ? (
 		<>
